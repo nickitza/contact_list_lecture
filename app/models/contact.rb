532a1b296d72365b59c3f550ac9a845c482a8ead
Nicki Nixon <nickitza@gmail.com>
2019-05-14 12:06:29 -0600

    add child model address

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ApplicationRecord
-  has_many :notes
+  # manually added:
+  has_many :notes, dependent: :destroy
+  has_one :address, dependent: :destroy
+  # rails doesn't assume that Contact can have multiple notes
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,6 @@
 class Note < ApplicationRecord
   #this line is what signifies that it is child to contact via:
   # $ rails g model Note body:text contact:belongs_to
+  # ^ that code is what told this file to know this
   belongs_to :contact
 end
